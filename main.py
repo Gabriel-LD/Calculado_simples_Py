4dd3cf71603685705f8a15873183bea62ecbfc3d
Gabriel Lima Degam <102440842+Gabriel-LD@users.noreply.github.com>
2022-11-14 19:09:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+operacao = input('Digite uma operação (+,-,*,/) a ser feita:')
+a = input('Digite um termo: ')
+b = input('Digite o segundo termo: ')
+if operacao == '+':
+  soma = int(a) + int(b)
+  print(soma)
+
+if operacao == '-':
+  subtracao = int(a) - int(b)
+  print(subtracao)
+
+if operacao == '*' :
+  multiplicacao = float(a) * float(b)
+  print(multiplicacao)
+
+if operacao == '/' :
+  divisao = float(a) / float(b)
+  print(divisao)
+
+else:
+  print('Erro, operaçao invalida')
+  
+  
